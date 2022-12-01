a26601140af82cc562f18ac2a39803e7d06fe3d5
Felipe Hernandez <felipehmorahds@gmail.com>
2022-12-01 11:58:56 -0400

    creando vista de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './plantillas/header/header.component';
 import { FooterComponent } from './plantillas/footer/footer.component';
-import { LoginComponent } from './vistas/login/login.component';
-import { DashboardComponent } from './vistas/dashboard/dashboard.component';
-import { NuevoComponent } from './vistas/nuevo/nuevo.component';
-import { EditarComponent } from './vistas/editar/editar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LoginComponent,
-    DashboardComponent,
-    NuevoComponent,
-    EditarComponent
+    routingComponents,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
